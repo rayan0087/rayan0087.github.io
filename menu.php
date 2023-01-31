49ec82e3654334f049ff9497875b2dcadcd947cb
rayan0087 <rayansarijatmiko@gmail.com>
2023-01-31 08:29:16 +0700

    set config file

---
+++ menu.php
@@ -0,0 +1,24 @@
+<header id="header">
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+        <img src="assets/img/profile-img.jpg" alt="" class="img-fluid rounded-circle">
+        <h1 class="text-light"><a href="index.html">Rahadyan Ghiffari J</a></h1>
+        <div class="social-links mt-3 text-center">
+          <a href="https://www.facebook.com/rahadyan.jatmiko.7/" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="https://www.instagram.com/rahadyangj07/" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="https://www.linkedin.com/in/rahadyan-ghiffari-jamtiko-a5249125a/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div>
+
+      <nav id="navbar" class="nav-menu navbar">
+        <ul>
+          <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
+          <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
+          <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
+          <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
+          <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+    </div>
+</header><!-- End Header -->
