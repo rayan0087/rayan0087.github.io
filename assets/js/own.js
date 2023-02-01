d1328a8c1b2143237732418d688307d2b93e4da4
rayan0087 <rayansarijatmiko@gmail.com>
2023-02-01 09:39:51 +0700

    push final

--- assets/js/own.js
+++ assets/js/own.js
@@ -1,3 +1,11 @@
+// menu portofolio
+const nav2 = document.querySelector('.menu-div-portofolio')
+fetch('menu-portofolio.html')
+.then(res=>res.text())
+.then(data=>{
+    nav2.innerHTML=data
+})
+
 // menu
 const nav = document.querySelector('.menu-div')
 fetch('menu.html')
